f9ae33bec554e54d41bb5b753857ac8af49f59a1
darlyn <darlynpadilla63@gmail.com>
2025-06-12 13:38:03 -0500

    fixed errors

--- src/Socket/groups.ts
+++ src/Socket/groups.ts
@@ -88,6 +88,7 @@ export const makeGroupsSocket = (config: SocketConfig) => {
 
 	return {
 		...sock,
+		groupQuery,
 		groupMetadata,
 		groupCreate: async (subject: string, participants: string[]) => {
 			const key = generateMessageIDV2()
