ecfa1173dac405daf0f654f55c97223222a55e25
darlyn <darlynpadilla63@gmail.com>
2025-06-13 20:43:07 -0500

    `Removed unused code and imports from groups.ts`

--- src/Socket/groups.ts
+++ src/Socket/groups.ts
@@ -7,11 +7,6 @@ import {
   WAMessageKey,
   WAMessageStubType,
 } from "../Types";
-import {
-  NewsletterMetadata,
-  NewsletterViewRole,
-  XWAPaths,
-} from "../Types/NewsLetter";
 import { generateMessageIDV2, unixTimestampSeconds } from "../Utils";
 import {
   BinaryNode,
@@ -19,30 +14,13 @@ import {
   getBinaryNodeChildren,
   getBinaryNodeChildString,
   jidEncode,
-  jidNormalizedUser,
-  S_WHATSAPP_NET,
+  jidNormalizedUser
 } from "../WABinary";
 import { makeChatsSocket } from "./chats";
 
-enum QueryIds {
-  JOB_MUTATION = "7150902998257522",
-  METADATA = "6620195908089573",
-  UNFOLLOW = "7238632346214362",
-  FOLLOW = "7871414976211147",
-  UNMUTE = "7337137176362961",
-  MUTE = "25151904754424642",
-  CREATE = "6996806640408138",
-  ADMIN_COUNT = "7130823597031706",
-  CHANGE_OWNER = "7341777602580933",
-  DELETE = "8316537688363079",
-  DEMOTE = "6551828931592903",
-}
-
 export const makeGroupsSocket = (config: SocketConfig) => {
   const sock = makeChatsSocket(config);
-  const { authState, ev, query, upsertMessage, generateMessageTag } = sock;
-
-  const encoder = new TextEncoder();
+  const { authState, ev, query, upsertMessage } = sock;
 
   const groupQuery = async (
     jid: string,
@@ -57,35 +35,6 @@ export const makeGroupsSocket = (config: SocketConfig) => {
         to: jid,
       },
       content,
-    });
-
-  const newsletterWMexQuery = async (
-    jid: string | undefined,
-    query_id: QueryIds,
-    content?: object
-  ) =>
-    query({
-      tag: "iq",
-      attrs: {
-        id: generateMessageTag(),
-        type: "get",
-        xmlns: "w:mex",
-        to: S_WHATSAPP_NET,
-      },
-      content: [
-        {
-          tag: "query",
-          attrs: { query_id },
-          content: encoder.encode(
-            JSON.stringify({
-              variables: {
-                newsletter_id: jid,
-                ...content,
-              },
-            })
-          ),
-        },
-      ],
     });
 
   const groupMetadata = async (jid: string) => {
@@ -147,24 +96,6 @@ export const makeGroupsSocket = (config: SocketConfig) => {
     ...sock,
     groupQuery,
     groupMetadata,
-    newsletterMetadata: async (
-      type: "invite" | "jid",
-      key: string,
-      role?: NewsletterViewRole
-    ) => {
-      const result = await newsletterWMexQuery(undefined, QueryIds.METADATA, {
-        input: {
-          key,
-          type: type.toUpperCase(),
-          view_role: role || "GUEST",
-        },
-        fetch_viewer_metadata: true,
-        fetch_full_image: true,
-        fetch_creation_time: true,
-      });
-
-      return extractNewsletterMetadata(result);
-    },
     groupCreate: async (subject: string, participants: string[]) => {
       const key = generateMessageIDV2();
       const result = await groupQuery("@g.us", "set", [
@@ -499,35 +430,5 @@ export const extractGroupMetadata = (result: BinaryNode) => {
     ),
     ephemeralDuration: eph ? +eph : undefined,
   };
-  return metadata;
-};
-
-export const extractNewsletterMetadata = (
-  node: BinaryNode,
-  isCreate?: boolean
-) => {
-  const result = getBinaryNodeChild(node, "result")?.content?.toString();
-  const metadataPath = JSON.parse(result!).data[
-    isCreate ? XWAPaths.CREATE : XWAPaths.NEWSLETTER
-  ];
-  console.log(metadataPath);
-  const metadata: NewsletterMetadata = {
-    id: metadataPath.id,
-    state: metadataPath.state.type,
-    creation_time: +metadataPath.thread_metadata.creation_time,
-    name: metadataPath.thread_metadata.name.text,
-    nameTime: +metadataPath.thread_metadata.name.update_time,
-    description: metadataPath.thread_metadata.description.text,
-    descriptionTime: +metadataPath.thread_metadata.description.update_time,
-    invite: metadataPath.thread_metadata.invite,
-    handle: metadataPath.thread_metadata.handle,
-    picture: metadataPath.thread_metadata.picture?.direct_path || null,
-    preview: metadataPath.thread_metadata.preview?.direct_path || null,
-    reaction_codes: metadataPath.thread_metadata.settings.reaction_codes.value,
-    subscribers: +metadataPath.thread_metadata.subscribers_count,
-    verification: metadataPath.thread_metadata.verification,
-    viewer_metadata: metadataPath.viewer_metadata,
-  };
-
   return metadata;
 };
