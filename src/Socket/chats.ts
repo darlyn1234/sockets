429784dd9645881a23d5c09513818a336cfb95cd
darlyn <darlynpadilla63@gmail.com>
2025-06-21 11:34:48 -0500

    refactor: migrate WASignalGroup to TypeScript and remove deprecated files (#1320) * refactor: migrate WASignalGroup to TypeScript and remove deprecated files
    * chore: remove WASignalGroup JavaScript files from package.json
    
    * refactor: update SenderKeyStore and SenderKeyStateStructure interfaces to export and add deserialize method. Fix types
    
    * chore: lint issue
    
    * refactor: improve constructor type checking and getSeed method in SenderChainKey
    
    * refactor: update key handling to use Buffer for improved consistency
    
    * signal: consistent naming and dir structure + add some missing types
    
    * fix: lint issues

--- src/Socket/chats.ts
+++ src/Socket/chats.ts
@@ -630,7 +630,7 @@ export const makeChatsSocket = (config: SocketConfig) => {
 			await sendNode({
 				tag: 'presence',
 				attrs: {
-					name: me.name,
+					name: me.name.replace(/@/g, ''),
 					type
 				}
 			})
